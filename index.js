143b70a127fbd7185eb5ac2d02a150e899c09769
Phil Barresi <me@philbarresi.com>
2018-02-18 09:28:49 -0500

    Created barebones proxy

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const axios = require("axios");
+const accountInstance = axios.create({
+  baseURL: "https://accounts.spotify.com/api/",
+  timeout: 1000
+});
+const apiInstance = axios.create({
+  baseURL: "https://api.spotify.com/",
+  timeout: 1000
+});
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,7 +20,47 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", function(req, res, next) {
+app.use("/api/token", async (req, res) => {
+  try {
+    const token = await accountInstance({
+      method: req.method,
+      url: "token",
+      data: "grant_type=client_credentials",
+      withCredentials: true,
+      headers: {
+        Authorization: req.headers.authorization
+      }
+    });
+
+    res.status(token.status);
+    res.json(token.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ failed: true });
+  }
+});
+
+app.use("*", async (req, res) => {
+  try {
+    const apiResult = await apiInstance({
+      method: req.method,
+      url: req.originalUrl,
+      data: req.body,
+      withCredentials: true,
+      headers: {
+        Authorization: req.headers.authorization
+      }
+    });
+
+    res.status(apiResult.status);
+    res.json(apiResult.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ failed: true });
+  }
+});
+
+app.get("*", function(req, res, next) {
   res.json({ working: true });
 });
 
